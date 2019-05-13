6bef31601cb2b610d42e992b41caf0783209c8ec
Jimmy Marmin <jimmy.marmin@gmail.com>
2019-05-13 14:53:37 +0200

    Création de la classe Eau

---
+++ tpThread/tpThread/Classes/Eau.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace tpThread.Classes
+{
+    class Eau
+    {
+        static int volume;
+
+        public Eau(int v)
+        {
+            volume = v;
+        }
+        
+    }
+}
